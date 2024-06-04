b77e79215c6fcdb7a1dc15af38626ca9c88877cc
Benjamin Gallusser <bgallusser@googlemail.com>
2024-06-04 19:41:08 +0200

    Clean up tests

--- src/napari_trackastra/_tests/test_demo_widget.py
+++ src/napari_trackastra/_tests/test_demo_widget.py
@@ -10,24 +10,11 @@ def test_demo_widget():
     viewer.add_image(img)
     viewer.add_labels(mask)
 
-    viewer.window.add_dock_widget(Tracker(viewer))
-
-
-def test_save():
-    viewer = napari.Viewer()
-    img, mask = example_data_bacteria()
-    viewer.add_image(img)
-    viewer.add_labels(mask)
-
     tracker = Tracker(viewer)
     viewer.window.add_dock_widget(tracker)
     tracker._run()
     tracker._save()
-    # import ipdb
-
-    # ipdb.set_trace()
 
 
 if __name__ == "__main__":
-    # test_demo_widget()
-    test_save()
+    test_demo_widget()
