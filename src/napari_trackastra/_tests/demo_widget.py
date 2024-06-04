33b468efb28fdadaf74e225b27e769aa6581a0fe
Benjamin Gallusser <bgallusser@googlemail.com>
2024-06-04 09:35:50 +0200

    Add test to CI

--- src/napari_trackastra/_tests/demo_widget.py
+++ src/napari_trackastra/_tests/demo_widget.py
@@ -2,10 +2,9 @@ import napari
 from trackastra.data import example_data_bacteria
 
 from napari_trackastra._widget import Tracker
-from trackastra.data import example_data_bacteria
 
 
-def demo_widget():
+def test_demo_widget():
     viewer = napari.Viewer()
     img, mask = example_data_bacteria()
     viewer.add_image(img)
@@ -15,6 +14,4 @@ def demo_widget():
 
 
 if __name__ == "__main__":
-    demo_widget()
-
-    napari.run()
+    test_demo_widget()
